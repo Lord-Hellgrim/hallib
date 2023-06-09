991d3e67ff81b3dd72e6dc123b87e2f6e860b86a
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-06-09 12:52:49 +0000

    -testing

--- gitterdone.sh
+++ gitterdone.sh
@@ -1,7 +1,11 @@
 git add .
+sleep 0.2
 git commit -m "$1"
+sleep 0.2
 git push -u origin master
+sleep 0.5
 lord-hellgrim
+sleep 0.5
 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
 echo "Pushed to git, probably"
 
