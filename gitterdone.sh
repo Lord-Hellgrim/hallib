537965c199c42a743506ee20446df0cb230af2f1
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-06-09 13:07:04 +0000

    -testing

--- gitterdone.sh
+++ gitterdone.sh
@@ -4,8 +4,10 @@ git commit -m "$1"
 sleep 0.2
 git push -u origin master
 sleep 0.5
-echo "lord-hellgrim"
+expect "Username for 'https://github.com':"
+send "lord-hellgrim"
 sleep 0.5
-echo "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"
+expect "Username for 'https://github.com':"
+send "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"
 echo "uploaded to github, probably"
 
