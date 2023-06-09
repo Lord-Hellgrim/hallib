1b0d3f7746ef0b2685e05f76708afabd171c7e2d
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-06-09 22:58:36 +0000

    -added_slicing

--- hallib.h
+++ hallib.h
@@ -1,26 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-char* string_sum(const char* str1, const char* str2) {
-    size_t len1 = strlen(str1);
-    size_t len2 = strlen(str2);
+// char* string_sum(const char* str1, const char* str2) {
+//     size_t len1 = strlen(str1);
+//     size_t len2 = strlen(str2);
 
-    char* result = (char*)malloc((len1 + len2 + 1) * sizeof(char));
+//     char* result = (char*)malloc((len1 + len2 + 1) * sizeof(char));
 
-    if (result == NULL) {
-        printf("Memory allocation failed.\n");
-        return NULL;
-    }
+//     if (result == NULL) {
+//         printf("Memory allocation failed.\n");
+//         return NULL;
+//     }
     
-    strcpy(result, str1);
-    strcat(result, str2);
+//     strcpy(result, str1);
+//     strcat(result, str2);
 
-    return result;
-}
+//     return result;
+// }
 
 void println(char* s) {
     
     printf("%s\n", s);
+
+}
+
+int stringlen(char* s) {
+    int i = 0;
+    while(s[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
+void string_slice(char* result, char* source, unsigned int start, unsigned int stop) {
+    // Result must have enough space to store the slice, so at least stop-start + 1
+    // Up to but not including!
+    if (stop > stringlen(source)) {
+        return;
+    } else if (start > stop) {
+        return;
+    } else if (start == stop) {
+        return;
+    }else {
+        int len = stop-start;
+        int i = start;
+        while (i < stop) {
+            result[i - start] = source[i];
+            i++;
+        }
+        result[len+1] = '\0';
+        return;
+    }
+    return;
+}
+
+// This one is a work in progress...
+void split_string(char* s, char split, void* fun) {
+    int len = stringlen(s);
+    int i = 0;
+    while(s[i] != '\0') {
+        if (s[i] == split) {
+            char* temp[i+1];
+            string_slice(temp, s, 0, i+1);
+            fun(temp);
+        }
+    }
 
 }
--- hallib_test.c
+++ hallib_test.c
@@ -2,5 +2,10 @@
 
 int main() {
     char* hello = "hello there, babygirl!";
+    int len = stringlen(hello);
     println(hello);
+    char slice[10];
+    string_slice(slice, hello, 6, 11);
+    println(slice);
+    printf("%d\n", len);
 }
