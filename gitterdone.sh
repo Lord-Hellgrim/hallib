e2f2041dc8ca44ee8b542626ada907ae7c2fc358
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-06-09 12:58:02 +0000

    -testing

--- gitterdone.sh
+++ gitterdone.sh
@@ -1,11 +1,8 @@
 git add .
-sleep 0.2
 git commit -m "$1"
-sleep 0.2
 git push -u origin master
-sleep 0.5
-lord-hellgrim
-sleep 0.5
-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
-echo "Pushed to git, probably"
+expect "Username for 'https://github.com':"
+send -- "lord-hellgrim"
+expect Password for "'https://lord-hellgrim@github.com': "
+send -- "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"
 
